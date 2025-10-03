e9a2b4de95b005bd55638bc3616136e3ba9be261
MUHAMMAD SHAFIQUE <zippyflash82@gmail.com>
2025-10-03 21:38:37 +0500

    home feed ui completed

--- lib/app/modules/main/controllers/main_controller.dart
+++ lib/app/modules/main/controllers/main_controller.dart
@@ -4,6 +4,13 @@ import 'package:only_u/app/services/auth_service.dart';
 class MainController extends GetxController {
   var isLoading = false.obs;
   final AuthService authService = AuthService();
+  var tabIndex = 0.obs;
+  var caroselIndex = 0.obs;
+  final List<String> caroselImages = [
+    'https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg',
+    'https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg',
+    'https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg',
+  ];
 
   Future<void> signOut() async {
     isLoading.value = true;
--- lib/app/modules/main/views/main_view.dart
+++ lib/app/modules/main/views/main_view.dart
@@ -1,36 +1,521 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
-import 'package:only_u/app/common/widgets/LoadingView.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:only_u/app/data/constants.dart';
 import '../controllers/main_controller.dart';
 
 class MainView extends GetView<MainController> {
   const MainView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('MainView'), centerTitle: true),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
 
-      body: Center(
-        child: Column(
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _buildAppBar(),
+                SizedBox(height: 20),
+                _buildSearchButton(),
+                SizedBox(height: 20),
+                _buildCategoryHorizontListView(),
+                SizedBox(height: 20),
+                Obx(() => _buildTabView()),
+                SizedBox(height: 20),
+                // BeautifulYoungBrunetteWomanWithHerHairPosingTraceryDress(),
+                SizedBox(height: 10),
+                _buildCaroselView(),
+                SizedBox(height: 20),
+                _buildHighlightView(),
+                SizedBox(height: 10),
+                PostView(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Main Landing Page - OnlyU', style: TextStyle(fontSize: 20)),
-            SizedBox(height: 20),
-            Obx(
-              () => controller.isLoading.value
-                  ? LoadingView()
-                  : TextButton(
-                      onPressed: () {
-                        controller.signOut();
-                      },
-                      child: Text('Logout'),
-                    ),
+            Text(
+              'Hello ,',
+              style: GoogleFonts.oleoScript(
+                textStyle: TextStyle(color: Color(0xFFFF3080), fontSize: 24),
+              ),
+            ),
+            Text(
+              'Esther Howard',
+              style: GoogleFonts.rubik(
+                textStyle: TextStyle(
+                  color: Color(0xFFE7F6FF),
+                  fontSize: 18,
+                  fontFamily: 'Rubik',
+                  height: 0,
+                ),
+              ),
             ),
           ],
         ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              width: 42,
+              height: 42,
+              child: Image.asset('assets/imgs/notification.png'),
+            ),
+            SizedBox(
+              width: 42,
+              height: 42,
+              child: Image.asset('assets/imgs/heart.png'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSearchButton() {
+    return SvgPicture.asset(
+      'assets/imgs/search_view.svg',
+      width: double.infinity,
+      height: 48,
+    );
+  }
+
+  Widget _buildCategoryHorizontListView() {
+    return SizedBox(
+      width: Get.size.width,
+      height: 42, // Set a fixed height for the horizontal ListView
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) =>
+            _buildCategoryIndividualView(index, 'Woman'),
+        separatorBuilder: (context, index) => Container(width: 5),
+        itemCount: 10,
+      ),
+    );
+  }
+
+  Widget _buildCategoryIndividualView(int index, String categoryName) {
+    if (index == 0) {
+      return SvgPicture.asset('assets/imgs/category_menu.svg', height: 40);
+    }
+
+    return Container(
+      width: 85,
+      height: 40,
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            width: 1,
+            strokeAlign: BorderSide.strokeAlignOutside,
+            color: Colors.white.withOpacity(0.10000000149011612),
+          ),
+          borderRadius: BorderRadius.circular(50),
+        ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Woman',
+            style: TextStyle(
+              color: Color(0xFFE7F6FF),
+              fontSize: 14,
+              fontFamily: 'Rubik',
+              height: 0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTabView() {
+    return Column(
+      children: [
+        Container(
+          height: 48,
+          padding: const EdgeInsets.symmetric(horizontal: 4),
+          decoration: ShapeDecoration(
+            color: Color(0x7F122C58),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(100),
+            ),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: InkWell(
+                  onTap: () {
+                    debugPrint("My Feed tapped");
+                    controller.tabIndex.value = 0;
+                  },
+                  child: Container(
+                    height: 42,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40,
+                      vertical: 10,
+                    ),
+                    decoration: controller.tabIndex.value == 0
+                        ? ShapeDecoration(
+                            color: Color(0x3FAC2458),
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                width: 1.50,
+                                color: Color(0xFFFF3080),
+                              ),
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          )
+                        : ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'My feed',
+                          style: TextStyle(
+                            color: Color(0xFFFFF7FA),
+                            fontSize: 16,
+                            fontFamily: 'Rubik',
+                            height: 0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () {
+                    debugPrint("All tapped");
+                    controller.tabIndex.value = 1;
+                  },
+                  child: Container(
+                    height: 42,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                    decoration: controller.tabIndex.value == 1
+                        ? ShapeDecoration(
+                            color: Color(0x3FAC2458),
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                width: 1.50,
+                                color: Color(0xFFFF3080),
+                              ),
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          )
+                        : ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'All',
+                          style: TextStyle(
+                            color: Color(0xFFE7F6FF),
+                            fontSize: 16,
+                            fontFamily: 'Rubik',
+                            height: 0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCaroselItem(String imagePath) {
+    return Stack(
+      children: [
+        Container(
+          width: 300,
+          height: 180,
+          decoration: ShapeDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(0.00, -1.00),
+              end: Alignment(0, 1),
+              colors: [Colors.black.withOpacity(0), Colors.black],
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(imagePath, fit: BoxFit.cover),
+          ),
+        ),
+        Positioned(
+          top: 10,
+          right: 20,
+          child: SizedBox(
+            height: 28,
+            width: 28,
+            child: Image.asset('assets/imgs/heart_white.png'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCaroselView() {
+    return Obx(
+      () => Column(
+        children: [
+          CarouselSlider(
+            items: [
+              _buildCaroselItem('assets/imgs/caurosel1.png'),
+              _buildCaroselItem('assets/imgs/caurosel2.png'),
+              _buildCaroselItem('assets/imgs/caurosel1.png'),
+            ],
+
+            // imageList.map((url) {
+            //   return Image.network(
+            //     url,
+            //     fit: BoxFit.cover,
+            //     width: double.infinity,
+            //   );
+            // }).toList(),
+            options: CarouselOptions(
+              height: 180,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              onPageChanged: (index, reason) {
+                controller.caroselIndex.value = index;
+              },
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: controller.caroselImages.asMap().entries.map((entry) {
+              return Container(
+                width: 10.0,
+                height: 10.0,
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: controller.caroselIndex.value == entry.key
+                      ? Color(0xFFFF3181)
+                      : Color(0xFFFF3181).withOpacity(0.3),
+                ),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHighlightView() {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        'Highlights',
+        style: GoogleFonts.oleoScript(
+          textStyle: TextStyle(color: Color(0xFFFFF7FA), fontSize: 32),
+        ),
+      ),
+    );
+  }
+}
+
+class PostView extends StatefulWidget {
+  const PostView({super.key});
+
+  @override
+  State<PostView> createState() => _PostViewState();
+}
+
+class _PostViewState extends State<PostView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      height: 430,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Color(0xFFFFFFFF).withOpacity(0.1),
+
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildUpperRow(),
+          _buildImageView(),
+          _buildUserNameTv(),
+          SizedBox(height: 5),
+          _buildDescriptionTv(),
+          SizedBox(height: 20),
+          _buildDivider(),
+          SizedBox(height: 20),
+          _buildBottomRow(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUpperRow() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 50,
+            width: 50,
+            child: Image.asset('assets/imgs/avator.png'),
+          ),
+          SizedBox(
+            // width: double.infinity,
+            child: Text(
+              'Brooklyn Simmons',
+              style: TextStyle(
+                color: Color(0xFFFFF7FA),
+                fontSize: 16,
+                fontFamily: 'Rubik',
+              ),
+            ),
+          ),
+          Spacer(),
+          SizedBox(
+            height: 30,
+            width: 30,
+            child: Image.asset('assets/imgs/heart_white.png'),
+          ),
+          // SizedBox(width: 20),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImageView() {
+    return SizedBox(
+      height: 240,
+      width: double.infinity,
+      child: Image.network(
+        'https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg',
+      ),
+    );
+  }
+
+  Widget _buildUserNameTv() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Text(
+        '@simmlove',
+        style: TextStyle(
+          color: secondaryColor,
+          fontSize: 14,
+          fontFamily: 'Rubik',
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDescriptionTv() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Text(
+        'Adoring the beauty of this simmm’s high neck white t-shirt with gorgeous quality while welcoming spring.',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 14,
+          fontFamily: 'Rubik',
+        ),
+        overflow: TextOverflow.clip,
+        maxLines: 1,
+      ),
+    );
+  }
+
+  Widget _buildDivider() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      width: double.infinity,
+      height: 1,
+      decoration: ShapeDecoration(
+        color: Colors.white.withOpacity(0.10000000149011612),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      ),
+    );
+  }
+
+  Widget _buildBottomRow() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
+          SvgPicture.asset('assets/imgs/like_white.svg'),
+          SizedBox(width: 5),
+          Text(
+            '4567',
+            style: TextStyle(
+              fontSize: 12,
+              fontFamily: 'Rubik',
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(width: 15),
+          SvgPicture.asset('assets/imgs/comment_white.svg'),
+          SizedBox(width: 5),
+          Text(
+            '13',
+            style: TextStyle(
+              fontSize: 12,
+              fontFamily: 'Rubik',
+              color: Colors.white,
+            ),
+          ),
+          Spacer(),
+          SvgPicture.asset('assets/imgs/send_white.svg'),
+        ],
       ),
     );
   }
